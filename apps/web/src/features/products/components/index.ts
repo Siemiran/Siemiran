fc92d3f76d299e75bfef7f88f21347180ae276a9
Ali Mahmoudi <siemiranco@gmail.com>
2026-07-31 16:01:17 +0330

    refactor(products): centralize component exports

---
+++ apps/web/src/features/products/components/index.ts
@@ -0,0 +1,11 @@
+export { default as ProductCard } from "./ProductCard";
+
+export { default as ProductImage } from "./ProductImage";
+
+export { default as ProductBadge } from "./ProductBadge";
+
+export { default as ProductMeta } from "./ProductMeta";
+
+export { default as ProductActions } from "./ProductActions";
+
+export { default as ProductSkeleton } from "./ProductSkeleton";
